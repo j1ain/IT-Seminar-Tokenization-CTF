76e1f194bd0b655415cc1933b511517843571e28
j1ain <jimenasuarezvaldesss@gmail.com>
2026-03-06 00:31:17 +0100

    TestCTF, added backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// This tells the server how to read the form data sent by the player
+app.use(express.urlencoded({ extended: true }));
+
+// When a player visits your site, send them your index.html file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// When a player submits a flag, run this check!
+app.post('/submit-flag', (req, res) => {
+    const userFlag = req.body.flag;
+    const correctFlag = "CTF{my_first_flag}"; // This is the secret password!
+
+    if (userFlag === correctFlag) {
+        res.send("<h1>Congratulations! You hacked it!</h1>");
+    } else {
+        res.send("<h1>Wrong flag. Try again!</h1><br><a href='/'>Go back</a>");
+    }
+});
+
+// Turn the server on
+app.listen(port, () => {
+    console.log(`Your CTF server is running at http://localhost:${port}`);
+});
